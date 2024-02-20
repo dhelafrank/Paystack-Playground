77321a328762b07352e7a7ac740a4461c8f282e4
dhelafrank <ubongabasijerome8@gmail.com>
2024-02-20 20:16:12 +0100

    Bug: Verification Route

--- routes/views.js
+++ routes/views.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const {homePage} = require('../controllers/views')
+const {
+    homePage
+} = require('../controllers/views')
 
 router.get('/', homePage);
+
+
+
+router.get('/ping', (req, res, next) => {
+res.send("Green!")
+})
+
 
 module.exports = router;
